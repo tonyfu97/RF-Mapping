14ba4d19c0c5289eb4e1d2962604b37430e1b71d
Tony Fu <tonyfu19971110@gmail.com>
2022-06-22 12:12:43 -0700

    adjust file paths

--- src/natural_images/gaussian_fit.py
+++ src/natural_images/gaussian_fit.py
@@ -1,33 +1,38 @@
 """
-Code to fit a 2D pixel array to a 2D Gaussian. 
+Code to fit a 2D pixel array to a 2D Gaussian.
 
 Tony Fu, June 17, 2022
 
 This code is the modified version of the code found on the following thread:
 https://stackoverflow.com/questions/21566379/fitting-a-2d-gaussian-function-using-scipy-optimize-curve-fit-valueerror-and-m
 """
+import os
+from PIL import Image
 
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from tqdm import tqdm
 
-def twoD_Gaussian(xycoord, amplitude, mu_x, mu_y, sigma_1, sigma_2, 
+
+def twoD_Gaussian(xycoord, amplitude, mu_x, mu_y, sigma_1, sigma_2,
                   theta, offset):
     """
-    The model function of a 2D Gaussian. Intended to be the first input 
+    The model function of a 2D Gaussian. Intended to be the first input
     argument for scipy.optimize.curve_fit(f, xdata, ydata).
 
     Parameters
     ----------
-    According to the official documentation of scipy.optimize.curve_fit(), the 
-    first argument of f (this function) should be the indepedent variable(s), 
+    According to the official documentation of scipy.optimize.curve_fit(), the
+    first argument of f (this function) should be the indepedent variable(s),
     and the rest of the arguments should be function parameters.
 
     xycoord: a two-tuple of 2D numpy arrays = (x, y)
-        The x and y coordinates, in unit pixels. x and y should be 2D array, 
+        The x and y coordinates, in unit pixels. x and y should be 2D array,
         i.e., the result of meshgrid(x, y).
     amplitude: int or float
-        The amplitude of the Gaussian, i.e., the max of the Gaussian if the 
+        The amplitude of the Gaussian, i.e., the max of the Gaussian if the
         offset is zero. Unitless.
     mu_x and mu_y: int or float
         The center coordiates of the Gaussian, in unit pixels.
@@ -35,7 +40,7 @@ def twoD_Gaussian(xycoord, amplitude, mu_x, mu_y, sigma_1, sigma_2,
         The std. dev. of the two orthogonal axis in unit pixels. sigma_1 should
         be the horizontal axis if theta = 0 degree.
     theta: int or float
-        The angle of the sigma_1 away from the positive x-axis, measured 
+        The angle of the sigma_1 away from the positive x-axis, measured
         counterclockwise in unit degrees.
     offset: int or float
         The offset of the Gaussian. Unitless.
@@ -44,7 +49,8 @@ def twoD_Gaussian(xycoord, amplitude, mu_x, mu_y, sigma_1, sigma_2,
     -------
     z: 2D numpy array
         The height of the 2D Guassian given coordinates (x, y). This has to be
-        an 1D array as per official documentation of scipy.optimize.curve_fit().
+        an 1D array as per official documentation of
+        scipy.optimize.curve_fit().
     """
     x, y = xycoord
     mu_x = float(mu_x)
@@ -54,8 +60,8 @@ def twoD_Gaussian(xycoord, amplitude, mu_x, mu_y, sigma_1, sigma_2,
     a = (np.cos(theta)**2)/(2*sigma_1**2) + (np.sin(theta)**2)/(2*sigma_2**2)
     b = -(np.sin(2*theta))/(4*sigma_1**2) + (np.sin(2*theta))/(4*sigma_2**2)
     c = (np.sin(theta)**2)/(2*sigma_1**2) + (np.cos(theta)**2)/(2*sigma_2**2)
-    z = offset + amplitude*np.exp( - (a*((x-mu_x)**2) + 2*b*(x-mu_x)*(y-mu_y) 
-                                      + c*((y-mu_y)**2)))
+    z = offset + amplitude*np.exp(-(a*((x-mu_x)**2) + 2*b*(x-mu_x)*(y-mu_y)
+                                  + c*((y-mu_y)**2)))
     return z.flatten()
 
 
@@ -80,7 +86,8 @@ def test_twoD_Gaussian():
 if __name__ == '__main__':
     test_twoD_Gaussian()
 
-def ellipticalfit(image, initial_guess=None, plot=True, show=False):
+
+def gaussian_fit(image, initial_guess=None, plot=True, show=False):
     """
     Fit a 2D gaussian to an input image.
 
@@ -94,20 +101,20 @@ def ellipticalfit(image, initial_guess=None, plot=True, show=False):
         Whether to plot the result isocline or not.
     plot: bool
         Whether to show the result plot.
-    
+
     Returns
     -------
     param_estimate: numpy.array consists of the following values
         amplitude: int or float
-            The amplitude of the Gaussian, i.e., the max of the Gaussian if the 
+            The amplitude of the Gaussian, i.e., the max of the Gaussian if the
             offset is zero. Unitless.
         mu_x and mu_y: int or float
             The center coordiates of the Gaussian, in unit pixels.
         sigma_1, sigma_2: int or float
-            The std. dev. of the two orthogonal axis in unit pixels. sigma_1 
+            The std. dev. of the two orthogonal axis in unit pixels. sigma_1
             should be the horizontal axis if theta = 0 degree.
         theta: int or float
-            The angle of the sigma_1 away from the positive x-axis, measured 
+            The angle of the sigma_1 away from the positive x-axis, measured
             counterclockwise in unit degrees.
         offset: int or float
             The offset of the Gaussian. Unitless.
@@ -126,13 +133,14 @@ def ellipticalfit(image, initial_guess=None, plot=True, show=False):
         sigma_2_guess = 30
         theta_guess = 90
         offset_guess = 0
-        initial_guess = (image.max(), x_size//2, y_size//2, 
-                         sigma_1_guess, sigma_2_guess, theta_guess, offset_guess)
-    
+        initial_guess = (image.max(), x_size//2, y_size//2,
+                         sigma_1_guess, sigma_2_guess,
+                         theta_guess, offset_guess)
+
     # Fit the 2D Gaussian.
-    param_estimate, params_covar = opt.curve_fit(twoD_Gaussian, (x, y), 
-                                                 image.flatten(), 
-                                                 p0=initial_guess, 
+    param_estimate, params_covar = opt.curve_fit(twoD_Gaussian, (x, y),
+                                                 image.flatten(),
+                                                 p0=initial_guess,
                                                  method='trf')
     param_sem = np.sqrt(np.diag(params_covar))
 
@@ -143,14 +151,14 @@ def ellipticalfit(image, initial_guess=None, plot=True, show=False):
         plt.imshow(image, cmap=plt.cm.jet)
         # plt.colorbar()
         plt.contour(x, y, image_fitted.reshape(y_size, x_size), 7, colors='w')
-    
+
     if show:
         plt.show()
 
     return param_estimate, param_sem
 
 
-def test_ellipticalfit():
+def test_gaussian_fit():
     """Test fit a 2D Gaussian."""
     # Create x and y indices.
     x_size, y_size = 200, 250
@@ -159,73 +167,94 @@ def test_ellipticalfit():
     x, y = np.meshgrid(x, y)
 
     # Create data.
-    data = twoD_Gaussian((x, y), 3, 80, 180, 40, 70, 45, 10).reshape(y_size, x_size)
-    data_noisy = data + 0.2 *np.random.normal(size=data.shape)
-        
+    data = twoD_Gaussian((x, y), 3, 80, 180, 40, 70, 45, 10).reshape(y_size,
+                                                                     x_size)
+    data_noisy = data + 0.2 * np.random.normal(size=data.shape)
+
     # Test fit.
-    param_estimate, param_sem = ellipticalfit(data_noisy, show=True)
+    param_estimate, param_sem = gaussian_fit(data_noisy, show=True)
     print(param_estimate)
     print(param_sem)
     assert np.all(param_sem < 1), 'SEM too high.'
 
 
 if __name__ == '__main__':
-    test_ellipticalfit()
+    test_gaussian_fit()
 
-"""
-Create a multi-page pdf file, each page contains the Gaussian fit of the 
-(average guided-backpropagation of the) best and the worst image patches for 
-each unit. 
 
-However, this code block only works for me. For other users, please change 
-the file paths, and make sure you have the .ppm files that contains the 
-average back-propagation of the image patches.
-"""
+def make_pdf(data_dir, best_file_names, worst_file_names, pdf_dir,
+             title="2D Gaussian of Average Backpropagation"):
+    """
+    Creates a multi-page pdf file, each page contains the Gaussian fit of the
+    (average guided-backpropagation of the) best and the worst image patches
+    for each unit.
 
-if __name__ == '__main__':
-    # Mount google drive in google colab
-    from google.colab import drive
-    drive.mount('/content/gdrive', force_remount=True)
-    import sys
-    sys.path.insert(0, '/content/gdrive/My Drive/Colab Notebooks/Border Ownership Research/conv2_ppm')
+    Parameters
+    ----------
+    data_dir : str
+        The directory to the data files.
+    best_file_names : list of str
+        The file names of the images  in data_path.
+    pdf_dir : str
+        The directory to save the result pdf, does not have to exist in the
+        first place.
+    title : str, optional
+        The title string on each pdf page.
+    """
+    pdf_path = os.path.join(pdf_dir, "gaussianFitResults.pdf")
 
-    # Other imports for plotting
-    from matplotlib.backends.backend_pdf import PdfPages
-    from PIL import Image
-    from tqdm import tqdm
-
-    with PdfPages("/content/gdrive/My Drive/Colab Notebooks/Border Ownership Research/test.pdf") as pdf:
-        num_units = 256
-        for i in tqdm(range(num_units)):
+    with PdfPages(pdf_path) as pdf:
+        page_count = 0
+        for best_file_name, worst_file_name in tqdm(zip(best_file_names,
+                                                        worst_file_names)):
             # Load the average back-propagation of the image patches.
-            best_file_name = f"b_conv2.{i}.mu.ppm"
-            best_avg_backprop = Image.open(dir + best_file_name)
+            best_file_path = os.path.join(data_dir, best_file_name)
+            best_avg_backprop = Image.open(best_file_path)
             best_avg_backprop_np = np.asarray(best_avg_backprop)
 
-            worst_file_name = f"w_conv2.{i}.mu.ppm"
-            worst_avg_backprop = Image.open(dir + worst_file_name)
+            worst_file_path = os.path.join(data_dir, worst_file_name)
+            worst_avg_backprop = Image.open(worst_file_path)
             worst_avg_backprop_np = np.asarray(worst_avg_backprop)
-            
+
             # Fit 2D Gaussian, and plot them.
-            plt.figure(figsize=(20,10))
-            plt.suptitle(f"2D Gaussian of Average Backpropagation: Conv2 No.{i}", fontsize=20)
-            plt.subplot(1,2,1)
-            params, sems = ellipticalfit(best_avg_backprop_np[:,:,0])
-            subtitle1 = f"A={params[0]:.2f}(err={sems[0]:.2f}), mu_x={params[1]:.2f}(err={sems[1]:.2f}), mu_y={params[2]:.2f}(err={sems[2]:.2f}),"
-            subtitle2 = f"sigma_1={params[3]:.2f}(err={sems[3]:.2f}), sigma_2={params[4]:.2f}(err={sems[4]:.2f}),"
-            subtitle3 = f"theta={params[5]:.2f}(err={sems[5]:.2f}), offset={params[6]:.2f}(err={sems[6]:.2f})"
-            # plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-            plt.title(f"Best Image Patches\n({subtitle1}\n{subtitle2}\n{subtitle3})", fontsize=14)
+            plt.figure(figsize=(20, 10))
+            plt.suptitle(f"{title}: unit no.{page_count}", fontsize=20)
+            page_count += 1
 
-            plt.subplot(1,2,2)
-            params, param_sem = ellipticalfit(worst_avg_backprop_np[:,:,0])
-            plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-            subtitle1 = f"A={params[0]:.2f}(err={sems[0]:.2f}), mu_x={params[1]:.2f}(err={sems[1]:.2f}), mu_y={params[2]:.2f}(err={sems[2]:.2f}),"
-            subtitle2 = f"sigma_1={params[3]:.2f}(err={sems[3]:.2f}), sigma_2={params[4]:.2f}(err={sems[4]:.2f}),"
-            subtitle3 = f"theta={params[5]:.2f}(err={sems[5]:.2f}), offset={params[6]:.2f}(err={sems[6]:.2f})"
-            # plt.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-            plt.title(f"Worst Image Patches\n({subtitle1}\n{subtitle2}\n{subtitle3})", fontsize=14)
+            plt.subplot(1, 2, 1)
+            params, sems = gaussian_fit(best_avg_backprop_np[:, :, 0])
+            subtitle1 = f"A={params[0]:.2f}(err={sems[0]:.2f}), "\
+                        f"mu_x={params[1]:.2f}(err={sems[1]:.2f}), "\
+                        f"mu_y={params[2]:.2f}(err={sems[2]:.2f}),"
+            subtitle2 = f"sigma_1={params[3]:.2f}(err={sems[3]:.2f}), "\
+                        f"sigma_2={params[4]:.2f}(err={sems[4]:.2f}),"
+            subtitle3 = f"theta={params[5]:.2f}(err={sems[5]:.2f}), "\
+                        f"offset={params[6]:.2f}(err={sems[6]:.2f})"
+            plt.title(f"Best Image Patches\n({subtitle1}\n{subtitle2}"\
+                      f"\n{subtitle3})", fontsize=14)
 
-            # Save the figure to pdf and close.
+            plt.subplot(1, 2, 2)
+            params, param_sem = gaussian_fit(worst_avg_backprop_np[:, :, 0])
+            subtitle1 = f"A={params[0]:.2f}(err={sems[0]:.2f}), "\
+                        f"mu_x={params[1]:.2f}(err={sems[1]:.2f}), "\
+                        f"mu_y={params[2]:.2f}(err={sems[2]:.2f}),"
+            subtitle2 = f"sigma_1={params[3]:.2f}(err={sems[3]:.2f}), "\
+                        f"sigma_2={params[4]:.2f}(err={sems[4]:.2f}),"
+            subtitle3 = f"theta={params[5]:.2f}(err={sems[5]:.2f}), "\
+                        f"offset={params[6]:.2f}(err={sems[6]:.2f})"
+            plt.title(f"Worst Image Patches\n({subtitle1}\n{subtitle2}"\
+                      f"\n{subtitle3})", fontsize=14)
+
             pdf.savefig()
             plt.close()
+
+
+if __name__ == '__main__':
+    repo_path = os.path.abspath(os.path.join(__file__, "../../.."))
+    data_dir = f"{repo_path}/data/conv2_ppm"
+    num_units = 256
+    best_file_names = [f"b_conv2.{i}.mu.ppm" for i in range(num_units)]
+    worst_file_names = [f"w_conv2.{i}.mu.ppm" for i in range(num_units)]
+    pdf_dir = f"{repo_path}/results"
+
+    make_pdf(data_dir, best_file_names, worst_file_names, pdf_dir)
