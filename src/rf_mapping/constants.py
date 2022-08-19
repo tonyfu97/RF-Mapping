ca25ea178dd66487fea34e2db9fa12c0f744d9b8
Tony Fu <61669380+tonyfu97@users.noreply.github.com>
2022-08-19 10:46:39 -0700

    Update constants.py

--- src/rf_mapping/constants.py
+++ src/rf_mapping/constants.py
@@ -1,4 +1,6 @@
-DEVICE = 'cuda'  # Use 'mps' if using Apple silicon, or 'cuda' if NVIDIA gpu.
+import torch
+# Use 'mps' if using Apple silicon
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 REPO_DIR = '/home/tfu/git_repos/borderownership'
 IMG_DIR = '/data/images_npy'
-# hi
+
