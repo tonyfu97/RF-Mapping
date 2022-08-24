4c6cd0603d354abc41bbf71ee8cb0f48c7317157
Tony Fu <tonyfu19971110@gmail.com>
2022-08-24 11:42:28 -0700

    include gt composite (max + min) maps in map correlations.py

--- src/rf_mapping/compare/map_correlations.py
+++ src/rf_mapping/compare/map_correlations.py
@@ -29,7 +29,7 @@ model_name = 'alexnet'
 # model_name = 'resnet18'
 image_shape = (227, 227)
 this_is_a_test_run = False
-max_or_min = 'min'
+max_or_min = 'max'
 font_size = 20
 r_val_threshold = 0.7
 
@@ -88,16 +88,20 @@ def load_maps(map_name, layer_name, max_or_min):
 def plot_r_val(r_val, p_val, font_size):
     plt.xticks([])
     plt.yticks([])
-    plt.text(0.2, 0.7, f"r = {r_val:.4f}", fontsize=font_size)
-    plt.text(0.1, 0.4, f"p = {p_val:.8f}", fontsize=font_size)
-    
+    plt.text(0.1, 0.7, f"r = {r_val:.4f}", fontsize=font_size)
+    plt.text(0.1, 0.4, f"p = {p_val * 100:.4f}%", fontsize=font_size)
+
 # Make pdf:
 for conv_i in range(num_layers):
     layer_name = f"conv{conv_i+1}"
     
-    high_r_val_counts = {'gt_vs_occlude' : 0,
+    high_r_val_counts = {'gt_vs_gt_composite' : 0,
+                         'gt_vs_occlude' : 0,
                          'gt_vs_rfmp4a' : 0,
                          'gt_vs_rfmp4c7o' : 0,
+                         'gt_composite_vs_occlude' : 0,
+                         'gt_composite_vs_rfmp4a' : 0,
+                         'gt_composite_vs_rfmp4c7o' : 0,
                          'occlude_vs_rfmp4a' : 0,
                          'occlude_vs_rfmp4c7o' : 0,
                          'rfmp4a_vs_rfmp4c7o' : 0}
@@ -106,6 +110,9 @@ for conv_i in range(num_layers):
     with PdfPages(pdf_path) as pdf:
         try:
             gt_maps = load_maps('gt', layer_name, max_or_min)
+            gt_max_maps = load_maps('gt', layer_name, 'max')
+            gt_min_maps = load_maps('gt', layer_name, 'min')
+            gt_max_min_maps = gt_max_maps + gt_min_maps
             occlude_maps = load_maps('occlude', layer_name, max_or_min)
             rfmp4a_maps = load_maps('rfmp4a', layer_name, max_or_min)
             rfmp4c7o_maps = load_maps('rfmp4c7o', layer_name, max_or_min) 
@@ -117,107 +124,144 @@ for conv_i in range(num_layers):
         # Correlate the maps.
         for unit_i in range(num_units):
             """
-                         gt  occlude  rfmp4a  rfmp4c7o         
-                          2      3       4         5
-                gt        7      8       9        10
-                occlude   12    13      14        15
-                rfmp4a    17    18      19        20
-                rfmp4c7o  22    23      24        25
+                              gt  gt(max+min) occlude  rfmp4a  rfmp4c7o         
+                               2      3         4         5       6
+                7. gt          8      9         10        11      12
+                13.gt(max+min) 14     15        16        17      18
+                19.occlude     20     21        22        23      24
+                25.rfmp4a      26     27        28        29      30
+                31.rfmp4c7o    32     33        34        35      36
             """
             plt.figure(figsize=(20, 18))
             plt.suptitle(f"Correlations of different maps (no.{unit_i}, {max_or_min})", fontsize=32)
 
-            plt.subplot(5, 5, 2)
+            plt.subplot(6, 6, 2)
             plt.imshow(gt_maps[unit_i]/gt_maps[unit_i].max(), cmap='gray')
             plt.title('gt', fontsize=font_size)
             
-            plt.subplot(5, 5, 3)
+            plt.subplot(6, 6, 3)
+            plt.imshow(gt_max_min_maps[unit_i]/gt_max_min_maps[unit_i].max(), cmap='gray')
+            plt.title('gt composite', fontsize=font_size)
+
+            plt.subplot(6, 6, 4)
             plt.imshow(occlude_maps[unit_i]/occlude_maps[unit_i].max(), cmap='gray')
             plt.title('occlude', fontsize=font_size)
             
-            plt.subplot(5, 5, 4)
+            plt.subplot(6, 6, 5)
             plt.imshow(rfmp4a_maps[unit_i]/rfmp4a_maps[unit_i].max(), cmap='gray')
             plt.title('rfmp4a', fontsize=font_size)
             
-            plt.subplot(5, 5, 5)
+            plt.subplot(6, 6, 6)
             plt.imshow(rfmp4c7o_maps[unit_i]/rfmp4c7o_maps[unit_i].max())
             plt.title('rfmp4c7o', fontsize=font_size)
             
-            plt.subplot(5, 5, 6)
+            plt.subplot(6, 6, 7)
             plt.imshow(gt_maps[unit_i]/gt_maps[unit_i].max(), cmap='gray')
             plt.ylabel('gt', fontsize=font_size)
             
-            plt.subplot(5, 5, 11)
+            plt.subplot(6, 6, 13)
+            plt.imshow(gt_max_min_maps[unit_i]/gt_max_min_maps[unit_i].max(), cmap='gray')
+            plt.ylabel('gt composite', fontsize=font_size)
+            
+            plt.subplot(6, 6, 19)
             plt.imshow(occlude_maps[unit_i]/occlude_maps[unit_i].max(), cmap='gray')
             plt.ylabel('occlude', fontsize=font_size)
             
-            plt.subplot(5, 5, 16)
+            plt.subplot(6, 6, 25)
             plt.imshow(rfmp4a_maps[unit_i]/rfmp4a_maps[unit_i].max(), cmap='gray')
             plt.ylabel('rfmp4a', fontsize=font_size)
             
-            plt.subplot(5, 5, 21)
+            plt.subplot(6, 6, 31)
             plt.imshow(rfmp4c7o_maps[unit_i]/rfmp4c7o_maps[unit_i].max())
             plt.ylabel('rfmp4c7o', fontsize=font_size)
             
             # Just like RFMP4c7o, but the color channels are averaged.
             rfmp4c7o_array = np.mean(rfmp4c7o_maps, axis=3)[unit_i].flatten()
             
-            plt.subplot(5, 5, 7)
+            plt.subplot(6, 6, 8)
             r_val, p_val = pearsonr(gt_maps[unit_i].flatten(), gt_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             
-            plt.subplot(5, 5, 8)
+            plt.subplot(6, 6, 9)
+            r_val, p_val = pearsonr(gt_maps[unit_i].flatten(), gt_max_min_maps[unit_i].flatten())
+            plot_r_val(r_val, p_val, font_size)
+            if r_val > r_val_threshold:
+                high_r_val_counts['gt_vs_gt_composite'] += 1
+            
+            plt.subplot(6, 6, 10)
             r_val, p_val = pearsonr(gt_maps[unit_i].flatten(), occlude_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['gt_vs_occlude'] += 1
             
-            plt.subplot(5, 5, 9)
+            plt.subplot(6, 6, 11)
             r_val, p_val = pearsonr(gt_maps[unit_i].flatten(), rfmp4a_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['gt_vs_rfmp4a'] += 1
             
-            plt.subplot(5, 5, 10)
+            plt.subplot(6, 6, 12)
             r_val, p_val = pearsonr(gt_maps[unit_i].flatten(), rfmp4c7o_array)
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['gt_vs_rfmp4c7o'] += 1
-            
-            plt.subplot(5, 5, 13)
+
+            plt.subplot(6, 6, 15)
+            r_val, p_val = pearsonr(gt_max_min_maps[unit_i].flatten(), gt_max_min_maps[unit_i].flatten())
+            plot_r_val(r_val, p_val, font_size)
+
+            plt.subplot(6, 6, 16)
+            r_val, p_val = pearsonr(gt_max_min_maps[unit_i].flatten(), occlude_maps[unit_i].flatten())
+            plot_r_val(r_val, p_val, font_size)
+            if r_val > r_val_threshold:
+                high_r_val_counts['gt_composite_vs_occlude'] += 1
+
+            plt.subplot(6, 6, 17)
+            r_val, p_val = pearsonr(gt_max_min_maps[unit_i].flatten(), rfmp4a_maps[unit_i].flatten())
+            plot_r_val(r_val, p_val, font_size)
+            if r_val > r_val_threshold:
+                high_r_val_counts['gt_composite_vs_rfmp4a'] += 1
+
+            plt.subplot(6, 6, 18)
+            r_val, p_val = pearsonr(gt_max_min_maps[unit_i].flatten(), rfmp4c7o_array)
+            plot_r_val(r_val, p_val, font_size)
+            if r_val > r_val_threshold:
+                high_r_val_counts['gt_composite_vs_rfmp4c7o'] += 1
+
+            plt.subplot(6, 6, 22)
             r_val, p_val = pearsonr(occlude_maps[unit_i].flatten(), occlude_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             
-            plt.subplot(5, 5, 14)
+            plt.subplot(6, 6, 23)
             r_val, p_val = pearsonr(occlude_maps[unit_i].flatten(), rfmp4a_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['occlude_vs_rfmp4a'] += 1
             
-            plt.subplot(5, 5, 15)
+            plt.subplot(6, 6, 24)
             r_val, p_val = pearsonr(occlude_maps[unit_i].flatten(), rfmp4c7o_array)
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['occlude_vs_rfmp4c7o'] += 1
             
-            plt.subplot(5, 5, 19)
+            plt.subplot(6, 6, 29)
             r_val, p_val = pearsonr(rfmp4a_maps[unit_i].flatten(), rfmp4a_maps[unit_i].flatten())
             plot_r_val(r_val, p_val, font_size)
             
-            plt.subplot(5, 5, 20)
+            plt.subplot(6, 6, 30)
             r_val, p_val = pearsonr(rfmp4a_maps[unit_i].flatten(), rfmp4c7o_array)
             plot_r_val(r_val, p_val, font_size)
             if r_val > r_val_threshold:
                 high_r_val_counts['rfmp4a_vs_rfmp4c7o'] += 1
             
-            plt.subplot(5, 5, 25)
+            plt.subplot(6, 6, 36)
             r_val, p_val = pearsonr(rfmp4c7o_array, rfmp4c7o_array)
             plot_r_val(r_val, p_val, font_size)
 
             pdf.savefig()
             plt.close()
         
-        plt.figure(figsize=(20, 10))
+        plt.figure(figsize=(20, 8))
         plt.bar(high_r_val_counts.keys(), high_r_val_counts.values())
         plt.ylabel('counts', fontsize=font_size)
         plt.title(f"Distribution of r values higher than {r_val_threshold}", fontsize=font_size)
